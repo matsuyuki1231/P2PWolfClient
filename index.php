4bb725a85214ea1b5bc4ee1f3339023d2cb45685
matsuyuki1231 <myp8721@gmail.com>
2024-06-01 15:36:57 +0900

    fix

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>YMGS Sync Communicator</title>
 <?php
+if (isset($_GET["serverId"])) {
+    setcookie("serverId", $_GET["serverId"], time() + 43200 /* 12時間 */);
+}
 if (!isset($_GET["code"])) {
     echo <<<PHP_EOL
 <meta http-equiv="refresh" content="0;URL=https://discord.com/oauth2/authorize?client_id=973860607969337344&response_type=code&redirect_uri=https%3A%2F%2Fvc-ymgs.f5.si&scope=identify">
